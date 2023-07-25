41da3cbe0a68ad1b39b523f4ffa57d6d2c1d96bf
peixinguo <peixin.guo24@gmail.com>
2023-07-26 00:01:31 +0800

    chore(managervue): 新增router、type依赖，用于路由，路径别名

---
+++ managervue/src/router/index.ts
@@ -0,0 +1,20 @@
+
+import {createRouter, createWebHistory, RouteRecordRaw} from "vue-router";
+
+export const constantRoutes: RouteRecordRaw[] = [
+    {
+        path: "/login",
+        name: "登录",
+        component:import("@/views/login/index.vue"),
+        meta: {
+            hidden: true
+        }
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: constantRoutes,
+  });
+  
+export default router;
---
+++ managervue/src/views/login/index.vue
@@ -0,0 +1,49 @@
+<template>
+    <h1>{{ title }}</h1>
+    <div v-if="noLogin">账号：<input v-model="username" type="text"/></div>
+    <div v-if="noLogin">密码：<input v-model="password" type="password"/></div>
+    <div v-on:click="click" style="border-radius: 30px;width: 100px;margin: 20px auto;color: white;background-color: blue;">
+        {{ buttonTitle }}
+    </div>
+    <input type="checkbox" v-model="checkbox"/>
+    <p>{{ checkbox }}</p>
+
+</template>
+<script lang="ts">
+
+    export default{
+        name: 'Login',
+        data:()=>({
+            title:'欢迎您，未登录',
+            username:'',
+            password:'',
+            buttonTitle:'登陆',
+            noLogin:true,
+            checkbox:true,
+        }),
+        methods:{
+           click(){
+            if(this.noLogin){
+                this.login()
+            }else{
+                this.logout()
+            }
+           },
+           login(){
+            alert('登陆成功');
+            this.title = '欢迎您'+this.username
+            this.noLogin = false;
+            this.buttonTitle = '退出登陆';
+           },
+           logout(){
+            this.noLogin = true;
+            this.title = '欢迎您，未登录'
+            this.buttonTitle = '登陆'
+           }
+        },
+    
+        
+    }
+
+    
+</script>
