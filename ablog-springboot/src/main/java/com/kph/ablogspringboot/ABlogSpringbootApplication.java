4e9460c02a1743a31b0d1c763c8a1abfdd719118
peixinguo <peixin.guo24@gmail.com>
2023-07-29 16:56:26 +0800

    feat(chore): 初始化后端工程

---
+++ ablog-springboot/src/main/java/com/kph/ablogspringboot/ABlogSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.kph.ablogspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ABlogSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ABlogSpringbootApplication.class, args);
+    }
+
+}
