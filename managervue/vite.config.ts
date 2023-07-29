9efeea9a4347e3aa39ce41e3a1c1a0fc74aa9bb4
peixinguo <peixin.guo24@gmail.com>
2023-07-29 16:42:30 +0800

    feat(managervue): 设置后端服务地址、端口

--- managervue/vite.config.ts
+++ managervue/vite.config.ts
@@ -1,15 +1,50 @@
+import AutoImport from "unplugin-auto-import/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import Components from "unplugin-vue-components/vite";
+import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 import { defineConfig } from 'vite'
+import { prismjsPlugin } from "vite-plugin-prismjs";
 import path from "path";
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+    createSvgIconsPlugin({
+      // 指定需要缓存的图标文件夹
+      iconDirs: [path.resolve(process.cwd(), "src/assets/icons")],
+      // 指定symbolId格式
+      symbolId: "icon-[dir]-[name]",
+    }),
+    prismjsPlugin({
+      languages: ["java", "python", "go", "html", "json"],
+      plugins: ["copy-to-clipboard"],
+      theme: "okaidia",
+      css: true,
+    }),
+  ],
   resolve:{
     alias:{
       "~": path.resolve(__dirname, "./"),
       "@": path.resolve(__dirname, "./src"),
     },
     extensions:[".mjs",".js", ".ts", ".jsx", ".tsx", ".json", ".vue"]
+  },
+  server: {
+    open: true,
+    proxy: {
+      "/api":{
+        target: "http://localhost:9080",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/,"")
+      }
+    }
   }
 })
