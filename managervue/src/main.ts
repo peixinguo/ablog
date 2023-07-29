cc45315863e97c4d14c8ff0b841ece5d58298c32
peixinguo <peixin.guo24@gmail.com>
2023-07-29 11:25:52 +0800

    feat(managervue): delete default css && Add global css configuration

--- managervue/src/main.ts
+++ managervue/src/main.ts
@@ -1,9 +1,10 @@
+import "@/assets/styles/index.scss";
 import { createApp } from 'vue'
-import './style.css'
 import SvgIcon from "@/components/SvgIcon/index.vue";
 import ElementPlus from "element-plus";
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 import router from "@/router";
+import VueViewer from "v-viewer";
 import App from './App.vue'
 
 const app = createApp(App);
@@ -12,6 +13,7 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component);
 }
 app.use(router);
+app.use(VueViewer);
 app.component("svg-icon", SvgIcon);
 app.use(ElementPlus);
 app.mount("#app");
